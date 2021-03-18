1f6b3edf0b684daad0d8fe91a30a413dc0dbbf53
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-19 00:48:14 +0100

    contact and work page structure

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="main text-right">
+      <h2>
+          Let's talk!
+      </h2>
+      <a id="contact-email" href="mailto:sofiasanchezurbano@gmail.com">Send me an email</a>
+      <a id="contact-cv" target="_blank" href="https://drive.google.com/file/d/19QalWKNHGzE8E_5ZyDkb3YecrlQc55GH/view?usp=sharing" download>Download Resume</a>
+      <a id="contact-github" href="https://github.com/SofSanUrb">Github</a>
+      <a id="contact-linkedin" href="https://www.linkedin.com/in/sofiasanchezurb/">LinkedIn</a>
+    </div>
+  );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import GoTop from '../components/GoTop'
 
 export default function Projects() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <>
+      <div className="main home text-left">
+        <h1>
+          Hi. I’m Sofia.<br></br>I am a Web developer.
+        </h1>
+      </div>
+      <div className="main home text-left">
+        <h1>
+          Hi. I’m Sofia.<br></br>I am a Web developer.
+        </h1>
+      </div>
+      <div className="main home text-left">
+        <h1>
+          Hi. I’m Sofia.<br></br>I am a Web developer.
+        </h1>
+      </div>
+      <GoTop scrollStepInPx="100" delayInMs="20.50"/>  
+    </>
+  );
 }
