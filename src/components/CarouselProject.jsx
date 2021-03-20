319a9f0c96334411884a489fbc560f2923b8a382
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-20 18:10:13 +0100

    projects mobile or desktop

--- src/components/CarouselProject.jsx
+++ src/components/CarouselProject.jsx
@@ -3,16 +3,23 @@ import Carousel from "react-bootstrap/Carousel";
 
 export default function CarouselProject(props) {
   const { projectImages } = props;
+  let mobileImages = projectImages.filter(image => image.includes('Mob'));
+  const mql = window.matchMedia('(max-width: 1050px)')
   return (
     <div className="column">
       <Carousel className="my-carousel">
-        {projectImages.map((imagePath, index) => {
-          return (
+        { mql.matches ? (mobileImages.map((imagePath, index) => { 
+            return (
             <Carousel.Item key={index}>
               <img src={imagePath} className="slide" alt="projectImage" />
             </Carousel.Item>
-          );
-        })}
+          )})) :  (projectImages.map((imagePath, index) => {
+            return (
+            <Carousel.Item key={index}>
+              <img src={imagePath} className="slide" alt="projectImage" />
+            </Carousel.Item>)
+          }))
+        }
       </Carousel>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,25 +4,44 @@ import GoTop from "../components/GoTop";
 import CarouselProject from "../components/CarouselProject";
 
 export default function Projects() {
-  const flatmeDetails = {
-    path: [
-      "./images/flatme_desk_1.PNG",
-      "./images/flatme_desk_2.PNG",
-      "./images/flatme_Mob_0.PNG",
-    ],
-    title: "flatme",
-    description:
-      "The Social Code (TSC) is a social media app (Full Stack Web App) where developers and technical people can share their experiences and create online events to network and share knowledge with other people",
-    github: "https://github.com/SofSanUrb/flatme" ,
-    live: "https://flatme.herokuapp.com/"
-  };
-  
-  const [projectFlatme, setFlatme] = useState(flatmeDetails);
+  const pathFlatme = [
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255056/flatme_desk_1_v5goji.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255055/flatme_desk_2_oqjglu.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255055/flatme_desk_3_jtyjkl.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255055/flatme_Mob_0_fizk7v.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255055/flatme_Mob_1_s1hg11.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255055/flatme_Mob_2_xwkzuz.png"
+  ];
+  const pathTSC = [
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255056/TSC_desk_1_mqos8r.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255299/TSC_Mob_1_hpxhjh.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255305/TSC_Mob_2_qc3msr.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255056/TSC_desk_3_fz6wla.png",
+    "https://res.cloudinary.com/dfpleoyv6/image/upload/v1616255056/TSC_Mob_5_tfbn64.png"
+  ];
+  const pathGame = [
+
+  ];
+  const pathSsurbano = [
+
+  ];
+
+  const [imagesflatme, setFlatme] = useState(pathFlatme);
+  const [imagesTSC, setTSC] = useState(pathTSC);
   return (
     <>
       <div className="main my-projects text-left">
         <h1>ssurbano Portfolio</h1>
         <p>Personal Website with my latest selected projects</p>
+        <div className="my-project-links">
+          <a className="text-blue" href="https://github.com/SofSanUrb/flatme">
+            GitHub
+          </a>
+          <a className="text-blue" href="https://flatme.herokuapp.com/">
+            Live Web
+          </a>
+        </div>
+        <CarouselProject projectImages={imagesflatme} />
       </div>
       <div className="main my-projects text-left">
         <h1>The Social Code</h1>
@@ -31,11 +50,26 @@ export default function Projects() {
           developers and technical people can share their experiences and create
           online events to network and share knowledge with other people
         </p>
+        <div className="my-project-links">
+          <a className="text-blue" href="https://github.com/marta8919/the-social-code-client">
+            Repo Client
+          </a>
+          <a className="text-blue" href="https://github.com/marta8919/the-social-code-server">
+            Repo Server
+          </a>
+          <a className="text-blue" href="https://the-social-code.herokuapp.com/">
+            Live Web
+          </a>
+        </div>
+        <CarouselProject projectImages={imagesTSC} />
       </div>
       <div className="main my-projects text-left">
-        <h1>{projectFlatme.title}</h1>
+        <h1>flatme</h1>
         <p>
-         {projectFlatme.description}
+          Readme is a Fullstack responsive social media web platform that gather
+          street and district reviews over different countries, with the goal of
+          helping people that need to move to an area or a city they don't know,
+          have as much information as possible.
         </p>
         <div className="my-project-links">
           <a className="text-blue" href="https://github.com/SofSanUrb/flatme">
@@ -45,10 +79,25 @@ export default function Projects() {
             Live Web
           </a>
         </div>
-        <CarouselProject projectImages={flatmeDetails.path} />
+        <CarouselProject projectImages={imagesflatme} />
       </div>
       <div className="main my-projects text-left">
         <h1>No Adults Allowed</h1>
+        <p>
+          Readme is a Fullstack responsive social media web platform that gather
+          street and district reviews over different countries, with the goal of
+          helping people that need to move to an area or a city they don't know,
+          have as much information as possible.
+        </p>
+        <div className="my-project-links">
+          <a className="text-blue" href="https://github.com/SofSanUrb/flatme">
+            GitHub
+          </a>
+          <a className="text-blue" href="https://flatme.herokuapp.com/">
+            Live Web
+          </a>
+        </div>
+        <CarouselProject projectImages={imagesflatme} />
       </div>
       <GoTop scrollStepInPx="100" delayInMs="20.50" />
     </>
