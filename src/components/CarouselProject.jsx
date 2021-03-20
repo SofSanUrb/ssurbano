99246b00082768d071db8c622625ee58b73cc310
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-20 18:42:16 +0100

    responsive carrousel

--- src/components/CarouselProject.jsx
+++ src/components/CarouselProject.jsx
@@ -3,23 +3,19 @@ import Carousel from "react-bootstrap/Carousel";
 
 export default function CarouselProject(props) {
   const { projectImages } = props;
-  let mobileImages = projectImages.filter(image => image.includes('Mob'));
-  const mql = window.matchMedia('(max-width: 1050px)')
+  
   return (
     <div className="column">
       <Carousel className="my-carousel">
-        { mql.matches ? (mobileImages.map((imagePath, index) => { 
-            return (
-            <Carousel.Item key={index}>
+        {projectImages.map((imagePath, index) => {
+            let deviceType = imagePath.includes('desk') ? 'desktop' : 'mobile'
+            
+          return (
+            <Carousel.Item key={index} className={deviceType}>
               <img src={imagePath} className="slide" alt="projectImage" />
             </Carousel.Item>
-          )})) :  (projectImages.map((imagePath, index) => {
-            return (
-            <Carousel.Item key={index}>
-              <img src={imagePath} className="slide" alt="projectImage" />
-            </Carousel.Item>)
-          }))
-        }
+          );
+        })}
       </Carousel>
     </div>
   );
