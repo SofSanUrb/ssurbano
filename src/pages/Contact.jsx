f98eddf55da104b5195ce25917c2deb43266207d
SofSanUrb <sofiasanchezurbano@gmail.com>
2021-03-25 19:12:09 +0100

    responsive changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,32 +4,34 @@ import Footer from "../components/Footer";
 
 export default function Contact() {
   return (
-    <div className="main text-right my-div-contact">
+    <><div className="main text-right my-div-contact">
       <Fade bottom>
-        <h2>Let's talk!</h2>
-        <div className="my-project-links my-contact">
-          <a id="contact-email" href="mailto:sofiasanchezurbano@gmail.com">
-            Send me an email
-          </a>
-          <a
-            id="contact-github"
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/SofSanUrb"
-          >
-            Github
-          </a>
-          <a
-            id="contact-linkedin"
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/sofiasanchezurb/"
-          >
-            LinkedIn
-          </a>
+        <div id="text-contact">
+          <h2>Let's talk!</h2>
+          <div className="my-project-links my-contact">
+            <a id="contact-email" href="mailto:sofiasanchezurbano@gmail.com">
+              Send me an email
+            </a>
+            <a
+              id="contact-github"
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/SofSanUrb"
+            >
+              Github
+            </a>
+            <a
+              id="contact-linkedin"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/sofiasanchezurb/"
+            >
+              LinkedIn
+            </a>
+          </div>
         </div>
       </Fade>
-      <Footer />
-    </div>
+      
+    </div><Footer /></>
   );
 }
